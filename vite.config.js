fe5923d978f87059d302befa1572aeb9a41804d0
Developer <test@example.com>
2026-08-30 02:16:21 +0300

    Fix build: install compatible Vite React plugin

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/NEW-RATIB/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
 })
